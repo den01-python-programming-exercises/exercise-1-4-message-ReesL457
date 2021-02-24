2c6220c8e1976debc0cf3ce1b2a65661eea9dbe8
ReesL457 <ReesL457@hwbcymru.net>
2021-02-24 16:08:43 +0000

    Input variable

--- src/exercise.py
+++ src/exercise.py
@@ -1,6 +1,8 @@
 def main():
     #write your code below this line
-    message = input("Write a message...")
+    input_string = input("Write a message...")
+    print(input_string)
 
 if __name__ == '__main__':
     main()
+
